ef21c2f94c2f08525bbeeb996f4da2e5efa1bf26
Brian Orimba <brianorimba@gmail.com>
2022-12-10 01:05:54 -0800

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,8 +19,7 @@ int main (void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	
+	n = rand() - RAND_MAX / 2;	
 	if (n > 0)
 	{
 	printf("%d is positive\n", n);
@@ -35,7 +34,4 @@ int main (void)
 	}
 	return (0);
 
-}	
-
-
-
+}
